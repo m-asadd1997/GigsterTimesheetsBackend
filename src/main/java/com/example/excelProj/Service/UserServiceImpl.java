a2c75c03ed27d30abb301ee5fc82b13cb052e212
m-asadd1997 <qureshiasad1000@gmail.com>
2020-03-18 00:24:11 +0500

    user crud done

--- src/main/java/com/example/excelProj/Service/UserServiceImpl.java
+++ src/main/java/com/example/excelProj/Service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.excelProj.Repository.UserDaoRepository;
 import com.example.excelProj.Commons.ApiResponse;
 import com.example.excelProj.Dto.UserDto;
 import com.example.excelProj.Model.User;
+
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -83,24 +85,52 @@ public class UserServiceImpl implements UserDetailsService {
         return userDto;
     }
 
-    public ApiResponse save(UserDto user) {
-		User founduser = userDaoRepository.findByEmail(user.getEmail());
-		if(founduser == null) {
+    public ApiResponse registerUser(UserDto userDto){
+		User founduser = userDaoRepository.findByEmail(userDto.getEmail());
+		User user1 = userDaoRepository.getAdminOfOrganization(userDto.getOrganizationName());
+		if(founduser == null && user1 == null) {
 			User newUser = new User();
-			newUser.setEmail(user.getEmail());
-			newUser.setName(user.getName());
-			newUser.setOrganizationName(user.getOrganizationName());
-			newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
-			newUser.setUserType(user.getUserType());
+			newUser.setEmail(userDto.getEmail());
+			newUser.setName(userDto.getName());
+			newUser.setOrganizationName(userDto.getOrganizationName());
+			newUser.setPassword(bcryptEncoder.encode(userDto.getPassword()));
+			newUser.setUserType(userDto.getUserType());
 			newUser.setActive(true);
-			if(user.getUserType().toLowerCase().equals("employee")  || user.getUserType().toLowerCase().equals("supervisor")){
-				trigerEmail(user.getEmail(),user.getPassword(),user.getUserType());
-			}
+
 			return new ApiResponse<>(HttpStatus.OK.value(), "User saved successfully.",	userDaoRepository.save(newUser));//return ;
-		}else{
+		}else if(founduser != null){
 			return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "User Already exsist.",null);//return ;
 		}
-    }
+		else if(user1 != null){
+			return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Organization Already exsist.",null);//return ;
+		}
+		else{
+			return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Error adding user",null);//return ;
+		}
+	}
+
+    public ApiResponse save(UserDto user) {
+		User founduser = userDaoRepository.findByEmail(user.getEmail());
+
+		if(founduser == null) {
+		User newUser = new User();
+		newUser.setEmail(user.getEmail());
+		newUser.setName(user.getName());
+		newUser.setOrganizationName(user.getOrganizationName());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		newUser.setUserType(user.getUserType());
+		newUser.setActive(true);
+		if(user.getUserType().toLowerCase().equals("employee")  || user.getUserType().toLowerCase().equals("supervisor")){
+			trigerEmail(user.getEmail(),user.getPassword(),user.getUserType());
+		}
+		return new ApiResponse<>(HttpStatus.OK.value(), "User saved successfully.",	userDaoRepository.save(newUser));//return ;
+	}else if(founduser != null){
+		return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "User Already exsist.",null);//return ;
+	}
+		else{
+		return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Error adding user",null);//return ;
+	}
+}
 
 
 	public List<User> getActiveUsers(Long id){
@@ -144,6 +174,38 @@ public class UserServiceImpl implements UserDetailsService {
 
 		javaMailSender.send(msg);
 
+	}
+
+	public ApiResponse<User> getUsersByOrganizationName(String organizationName){
+		List<User> users = userDaoRepository.getUsersByOrganizationName(organizationName);
+		if(users != null){
+			return new ApiResponse<>(200,"Users found",users);
+		}
+		else{
+			return new ApiResponse<>(404,"Users not found",null);
+		}
+	}
+
+	public ApiResponse deleteUser(Long id){
+		Optional<User> user = userDaoRepository.findById(id);
+		if(user.isPresent()){
+			userDaoRepository.deleteById(id);
+			return new ApiResponse(200,"User Deleted successfully",null);
+		}
+		else{
+			return new ApiResponse(404,"User not found",null);
+		}
+	}
+
+	public ApiResponse<User> getUserById(Long id){
+		Optional<User> user = userDaoRepository.findById(id);
+		if(user.isPresent()){
+
+			return new ApiResponse<>(200,"User found",user.get());
+		}
+		else{
+			return new ApiResponse<>(404,"User not found",null);
+		}
 	}
 
 }
